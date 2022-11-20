d35ab1b3121c76dcb306c56f408d0f1f255a3b7b
jckamaicha <theboringschool.org@gmail.com>
2022-11-20 23:58:37 +0545

    Disabled Button

--- src/components/PredictionTabs/components/TabByPool.jsx
+++ src/components/PredictionTabs/components/TabByPool.jsx
@@ -252,7 +252,7 @@ const PoolType = ({
             {/* @note must get balance from user wallet balance */}
             <h4>Available: $10</h4>
           </div>
-          <Button onClick={() => handlePredction()}>Predict</Button>
+          <Button disabled={true} onClick={() => handlePredction()}>Predict</Button>
         </div>
       </div>
     </>
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -16,6 +16,32 @@ export default function Home({setActiveFixtureBackground}) {
 
   return (
     <div className="home__container">
+      <div className="marketingBanners__container">
+        <h1>Explore endless possibilities with Playpoint.</h1>
+        <div className="marketingBanners__items">
+          <div className="marketingBanners__item">
+            <img src="https://img.freepik.com/free-vector/employee-working-office-interior-workplace-flat-vector-illustration_1150-37453.jpg?w=826&t=st=1664647893~exp=1664648493~hmac=1edcd93a5a19f365559dda8959dfe3f9b3b4805d94aff0db6d38eb2c13e07f67" alt="" />
+            <h2>SELECT THE POOL </h2>
+            <p>
+            Select the Pool of League you would want to predict in. <br/>
+Click on quick view of the match you want to play and predict. <br/>
+Select the desirable Wager Stakes per unit entry options.
+              </p> </div>
+          <div className="marketingBanners__item">
+            <h2>MAKE YOUR PREDICTION</h2>
+            <p>
+            Answer the four questions. <br/>Each questions have been marked points.<br/>Once the scheduled selected match kicks off the real-time show concludes.
+            </p>
+            <img src="https://cdni.iconscout.com/illustration/premium/thumb/future-prediction-by-employer-4252956-3526673.png" style={{scale:"1"}} alt="" />
+          </div>
+          <div className="marketingBanners__item">
+            <img src="https://img.freepik.com/premium-vector/money-growth-success-progress-report-vector-illustration-calculating-measuring-company-sales-improvement-statistics_610956-1547.jpg?w=2000" alt="" />
+            <h2>HIGHEST POINT WINS</h2>
+            <p>After systematic scores computational procedures, the winners are determined and sweepstakes are awarded in accordance.</p>
+          </div>
+        </div>
+      </div>
+
       <div className="marketplace__items">
         {marketplaceItems.length >= 1 ? (
           marketplaceItems.map((marketplace, index) => {
@@ -56,31 +82,6 @@ export default function Home({setActiveFixtureBackground}) {
             })}
           </>
         )}
-      </div>
-      <div className="marketingBanners__container">
-        <h1>Explore endless possibilities with Playpoint.</h1>
-        <div className="marketingBanners__items">
-          <div className="marketingBanners__item">
-            <img src="https://img.freepik.com/free-vector/employee-working-office-interior-workplace-flat-vector-illustration_1150-37453.jpg?w=826&t=st=1664647893~exp=1664648493~hmac=1edcd93a5a19f365559dda8959dfe3f9b3b4805d94aff0db6d38eb2c13e07f67" alt="" />
-            <h2>SELECT THE POOL </h2>
-            <p>
-            Select the Pool of League you would want to predict in. <br/>
-Click on quick view of the match you want to play and predict. <br/>
-Select the desirable Wager Stakes per unit entry options.
-              </p> </div>
-          <div className="marketingBanners__item">
-            <h2>MAKE YOUR PREDICTION</h2>
-            <p>
-            Answer the four questions. <br/>Each questions have been marked points.<br/>Once the scheduled selected match kicks off the real-time show concludes.
-            </p>
-            <img src="https://cdni.iconscout.com/illustration/premium/thumb/future-prediction-by-employer-4252956-3526673.png" style={{scale:"1"}} alt="" />
-          </div>
-          <div className="marketingBanners__item">
-            <img src="https://img.freepik.com/premium-vector/money-growth-success-progress-report-vector-illustration-calculating-measuring-company-sales-improvement-statistics_610956-1547.jpg?w=2000" alt="" />
-            <h2>HIGHEST POINT WINS</h2>
-            <p>After systematic scores computational procedures, the winners are determined and sweepstakes are awarded in accordance.</p>
-          </div>
-        </div>
       </div>
 
     </div>
