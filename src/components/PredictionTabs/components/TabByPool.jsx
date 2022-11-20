d365107fdc0b7b614c52c480a046269da4ab3e63
sudeepmi <sudeepm0599@gmail.com>
2022-11-20 21:45:25 +0545

    bug fixed

--- src/components/PredictionTabs/components/TabByPool.jsx
+++ src/components/PredictionTabs/components/TabByPool.jsx
@@ -209,7 +209,7 @@ const PoolType = ({
                           <input
                             style={{ padding: "5px 10px" }}
                             type="text"
-                            value={""}
+                            value={_predictionData.answers[index]}
                             placeholder={"Your Answer..."}
                             onChange={(e) =>
                               handleRadioChange(index, e.target.value)
