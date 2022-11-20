5e73f85d0a706fc82169b0ccaf4910280db9e6c8
sudeepmi <sudeepm0599@gmail.com>
2022-11-20 15:17:10 +0545

    profile page

---
+++ src/pages/profile/index.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import "./styles/style.css";
+import { Button } from "@mui/material";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import { Helmet } from "react-helmet";
+import UserHistory from "../../mocks/UserHistory.json";
+
+export default function Profile() {
+  return (
+    <div className="profile__container">
+      <Helmet>
+        <title>Profile | Playpoint</title>
+      </Helmet>
+      <div className="blob1"></div>
+      <div className="summary__container"  >
+        <div className="summaryItem">
+          <i className="ri-money-dollar-circle-line"></i>
+          <div>
+            <p>Your money in pool</p>
+            <h3>6676.00 PPTT</h3>
+          </div>
+        </div>
+        <div className="summaryItem">
+          <i className="ri-bar-chart-grouped-line"></i>
+          <div>
+            <p>Winnings of all time</p>
+            <h3>5896.00 PPTT</h3>
+          </div>
+        </div>
+        <div className="summaryItem">
+          <i className="ri-wallet-3-line"></i>
+          <div>
+            <p>Your balance</p>
+            <h3>589658.00 PPTT</h3>
+          </div>
+        </div>
+      </div>
+
+      <div className="userData">
+        <img
+          src="https://images.unsplash.com/photo-1579952363873-27f3bade9f55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80"
+          alt=""
+          loading="lazy"
+        />
+        <Button>
+          <i className="ri-add-box-line"></i> Add Money
+        </Button>
+      </div>
+
+      <div className="history__container">
+        <div className="titles">
+          <p>ID</p>
+          <p>Result</p>
+          <p>amount</p>
+          <p>win/lose amount</p>
+          <p>match</p>
+          <p>date/time</p>
+        </div>
+
+        <div className="history__items">
+          {UserHistory.map((data, index) => (
+            <div className="history__item" key={index}>
+              <p>{data.id}</p>
+              <p className={data.result}>
+                <span>{data.result}</span>
+              </p>
+              <p>
+                ${data.amount}~{data.amount} PPTT
+              </p>
+              <p className={data.result}>
+                {data.result === "win" || data.result === "lose" ? (
+                  <>
+                    ${data.resultAmount}~{data.resultAmount * 0.015} PPTT
+                  </>
+                ) : (
+                  <></>
+                )}
+              </p>
+              <p>
+                <b>{data.match.home}</b> VS <b>{data.match.away}</b>
+              </p>
+              <p>{data.date}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <Stack spacing={2}>
+        <Pagination count={10} shape="rounded" />
+      </Stack>
+    </div>
+  );
+}
--- src/utils/Routers.jsx
+++ src/utils/Routers.jsx
@@ -4,6 +4,7 @@ import Fixture from "../pages/fixture";
 import Predict from "../pages/predict";
 import NotFound from "../pages/404";
 import Home from "../pages/home";
+import Profile from "../pages/profile";
 
 import socketIO from "socket.io-client";
 const socket = socketIO.connect(import.meta.env.VITE_SOCKET_URL);
@@ -19,6 +20,12 @@ export default function PageRouters() {
         path="/"
         element={
           <Home setActiveFixtureBackground={setActiveFixtureBackground} />
+        }
+      />
+       <Route
+        path="/profile"
+        element={
+          <Profile setActiveFixtureBackground={setActiveFixtureBackground} />
         }
       />
       <Route
