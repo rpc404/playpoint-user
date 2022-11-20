f0ba4dca2e933c7fc560e3cf03067e8f07700035
jckamaicha <theboringschool.org@gmail.com>
2022-11-20 23:53:10 +0545

    Update

--- src/pages/predict/index.jsx
+++ src/pages/predict/index.jsx
@@ -75,9 +75,7 @@ export default function Predict({ socket }) {
       const response = await getQuestionaireByFixtureId(fixtureId);
       setQuestionaires(response.data.data);
     })();
-
   }, []);
-
   return (
     <div className="prediction__container">
       <Helmet>
@@ -213,10 +211,7 @@ export default function Predict({ socket }) {
             </p>
           </div>
           <div className={`leaderboardItems ${activeOS}`}>
-            {[
-              0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
-              19, 20, 21, 22, 23, 24,
-            ].map((data) => {
+            {/* {fixture[0].length >=1 && fixture.map((data) => {
               return (
                 <div className="leaderboardItem__container" key={data}>
                   <p>AUS/QTR</p>
@@ -224,7 +219,7 @@ export default function Predict({ socket }) {
                   <p>$432k</p>
                 </div>
               );
-            })}
+            })} */}
           </div>
         </div>
       </div>
