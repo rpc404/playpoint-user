f60d15376fce124efd863b21dfa651d8ebc97070
jckamaicha <theboringschool.org@gmail.com>
2022-11-21 17:04:58 +0545

    Making date formats

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,7 +4,7 @@ import "./styles/style.css";
 import MarketplaceCard from "../../components/MarketplaceCard";
 import { getMarketplaces } from "../../api/Marketplace";
 
-export default function Home({setActiveFixtureBackground}) {
+export default function Home({ setActiveFixtureBackground }) {
   const [marketplaceItems, setMarketplaceItems] = React.useState([]);
 
   React.useEffect(() => {
@@ -20,7 +20,13 @@ export default function Home({setActiveFixtureBackground}) {
       <div className="marketplace__items">
         {marketplaceItems.length >= 1 ? (
           marketplaceItems.map((marketplace, index) => {
-            return <MarketplaceCard setActiveFixtureBackground={setActiveFixtureBackground} marketplace={marketplace} key={index} />;
+            return (
+              <MarketplaceCard
+                setActiveFixtureBackground={setActiveFixtureBackground}
+                marketplace={marketplace}
+                key={index}
+              />
+            );
           })
         ) : (
           <>
@@ -30,7 +36,7 @@ export default function Home({setActiveFixtureBackground}) {
                   <Skeleton
                     animation="wave"
                     variant="rectangular"
-                    width={window.innerWidth < 576 ? "80vw" :"20vw"}
+                    width={window.innerWidth < 576 ? "80vw" : "20vw"}
                     height={"20vh"}
                   />
                   <div
@@ -58,39 +64,66 @@ export default function Home({setActiveFixtureBackground}) {
           </>
         )}
       </div>
-      <hr />
+      <div className="divider"></div>
+
       <div className="marketingBanners__container">
         <h1>Explore endless possibilities with Playpoint.</h1>
         <div className="marketingBanners__items">
           <div className="marketingBanners__item">
-            <img src="https://img.freepik.com/free-vector/employee-working-office-interior-workplace-flat-vector-illustration_1150-37453.jpg?w=826&t=st=1664647893~exp=1664648493~hmac=1edcd93a5a19f365559dda8959dfe3f9b3b4805d94aff0db6d38eb2c13e07f67" alt="" />
+            <img
+              src="https://img.freepik.com/free-vector/employee-working-office-interior-workplace-flat-vector-illustration_1150-37453.jpg?w=826&t=st=1664647893~exp=1664648493~hmac=1edcd93a5a19f365559dda8959dfe3f9b3b4805d94aff0db6d38eb2c13e07f67"
+              alt=""
+            />
             <h2>SELECT THE POOL </h2>
             <p>
-            Select the Pool of League you would want to predict in. <br/>
-Click on quick view of the match you want to play and predict. <br/>
-Select the desirable Wager Stakes per unit entry options.
-              </p> </div>
+              Select the Pool of League you would want to predict in. <br />
+              Click on quick view of the match you want to play and predict.{" "}
+              <br />
+              Select the desirable Wager Stakes per unit entry options.
+            </p>{" "}
+          </div>
           <div className="marketingBanners__item">
             <h2>MAKE YOUR PREDICTION</h2>
             <p>
-            Answer the four questions. <br/>Each questions have been marked points.<br/>Once the match kicks off the predict closes.
+              Answer the four questions. <br />
+              Each questions have been marked points.
+              <br />
+              Once the match kicks off the predict closes.
             </p>
-            <img src="https://cdni.iconscout.com/illustration/premium/thumb/future-prediction-by-employer-4252956-3526673.png" style={{scale:"1"}} alt="" />
+            <img
+              src="https://cdni.iconscout.com/illustration/premium/thumb/future-prediction-by-employer-4252956-3526673.png"
+              style={{ scale: "1" }}
+              alt=""
+            />
           </div>
           <div className="marketingBanners__item">
-            <img src="https://img.freepik.com/premium-vector/money-growth-success-progress-report-vector-illustration-calculating-measuring-company-sales-improvement-statistics_610956-1547.jpg?w=2000" alt="" />
+            <img
+              src="https://img.freepik.com/premium-vector/money-growth-success-progress-report-vector-illustration-calculating-measuring-company-sales-improvement-statistics_610956-1547.jpg?w=2000"
+              alt=""
+            />
             <h2>HIGHEST POINT WINS</h2>
-            <p>After systematic scores computational procedures, the winners are determined and sweepstakes are awarded in accordance.</p>
+            <p>
+              After systematic scores computational procedures, the winners are
+              determined and sweepstakes are awarded in accordance.
+            </p>
           </div>
         </div>
       </div>
+
+      <div className="divider"></div>
 
       <h1 className="home__mainTitle">Active Marketplaces</h1>
 
       <div className="marketplace__items">
         {marketplaceItems.length >= 1 ? (
           marketplaceItems.map((marketplace, index) => {
-            return <MarketplaceCard setActiveFixtureBackground={setActiveFixtureBackground} marketplace={marketplace} key={index} />;
+            return (
+              <MarketplaceCard
+                setActiveFixtureBackground={setActiveFixtureBackground}
+                marketplace={marketplace}
+                key={index}
+              />
+            );
           })
         ) : (
           <>
@@ -100,7 +133,7 @@ Select the desirable Wager Stakes per unit entry options.
                   <Skeleton
                     animation="wave"
                     variant="rectangular"
-                    width={window.innerWidth < 576 ? "80vw" :"20vw"}
+                    width={window.innerWidth < 576 ? "80vw" : "20vw"}
                     height={"20vh"}
                   />
                   <div
@@ -128,7 +161,6 @@ Select the desirable Wager Stakes per unit entry options.
           </>
         )}
       </div>
-
     </div>
   );
 }
