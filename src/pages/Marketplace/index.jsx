532506ed8d2cb9ff824a0fefca0872a165431b4a
surajgaire14 <eriag321@gmail.com>
2022-11-29 13:57:39 +0545

    marketplace leaderboard made

---
+++ src/pages/Marketplace/index.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import MarketplaceCard from "../../components/MarketplaceCard";
+import { useMarketplaceContext } from "../../contexts/Marketplace/MarketplaceContext";
+import { getMarketplaces, getMarketplaceStat } from "../../api/Marketplace";
+import { ACTIONS } from "../../contexts/Marketplace/MarketplaceReducer";
+import { Skeleton, Stack } from "@mui/material";
+import "./styles/style.css";
+
+const MarketPlace = () => {
+  const [{ marketplaces }, dispatchMarketplaceData] = useMarketplaceContext();
+  const [loading, setLoading] = React.useState(true);
+
+  console.log(marketplaces);
+
+  React.useEffect(() => {
+    (async () => {
+      if (marketplaces.length === 0) {
+        let res = await getMarketplaces();
+        res = res.data.marketplaces;
+
+        dispatchMarketplaceData({
+          type: ACTIONS.SET_ALL_MARKETPLACE,
+          payload: res,
+        });
+      }
+      setLoading(false);
+    })();
+  }, []);
+
+  return (
+    <div className="marketplace__container">
+      <div className="marketplace__items">
+        {marketplaces && marketplaces.length >= 1 && !loading ? (
+          marketplaces.map((marketplace, index) => {
+            return <MarketplaceCard marketplace={marketplace} key={index} />;
+          })
+        ) : (
+          <div id="skeleton__container">
+            {[0, 1, 2, 3, 4].map((skeleton) => {
+              return (
+                <Stack key={skeleton} className="stack">
+                  <Skeleton
+                    animation="wave"
+                    variant="rectangular"
+                    height={"16vh"}
+                    className={"skeleton"}
+                  />
+                  <Skeleton width={150} height={30} />
+                  <div className="inner__skeletons">
+                    {[0, 1, 2].map((skeleton) => {
+                      return <Skeleton width={40} height={40} key={skeleton} />;
+                    })}
+                  </div>
+                </Stack>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MarketPlace;
--- src/utils/Routers.jsx
+++ src/utils/Routers.jsx
@@ -5,31 +5,20 @@ import Predict from "../pages/predict";
 import NotFound from "../pages/404";
 import Home from "../pages/home";
 import Profile from "../pages/profile";
+import MarketPlace from "../pages/Marketplace";
 
 // import socketIO from "socket.io-client";
 // const socket = socketIO.connect(import.meta.env.VITE_SOCKET_URL);
 
-export default function PageRouters({socket}) {
+export default function PageRouters({ socket }) {
   return (
     <Routes>
       <Route path="*" element={<NotFound />} />
-      <Route
-        path="/"
-        element={
-          <Home />
-        }
-      />
-      <Route
-        path="/profile"
-        element={
-          <Profile />
-        }
-      />
-      <Route
-        path="fixture"
-        element={<Fixture />}
-      />
+      <Route path="/" element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="fixture" element={<Fixture />} />
       <Route path="predict/:fixtureId" element={<Predict socket={socket} />} />
+      <Route path="marketplace" element={<MarketPlace />} />
     </Routes>
   );
 }
