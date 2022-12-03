0e48e28581a0956a04c8ee3801f8f8bdfb5881c6
jckamaicha <theboringschool.org@gmail.com>
2022-12-04 00:04:08 +0545

    Designed Leaderboards

---
+++ src/pages/Leaderboard/components/FixtureTable/index.jsx
@@ -0,0 +1,94 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Link } from "react-router-dom";
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
+
+export default function FixtureTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Rank</TableCell>
+            <TableCell>Fixture</TableCell>
+            <TableCell align="center">Marketplace</TableCell>
+            <TableCell align="center">Most Active User</TableCell>
+            <TableCell align="center">Game Information</TableCell>
+            <TableCell align="center">Total Users</TableCell>
+            <TableCell align="center">Total Volume</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row, i) => (
+            <TableRow
+              key={i}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {i + 1}
+              </TableCell>
+              <TableCell>
+                <div className="fixtureContent">
+                  <div className="homeTeam">
+                    Brazil{" "}
+                    <img
+                      src="https://cdn.jsdelivr.net/npm/country-flag-emoji-json@2.0.0/dist/images/BR.svg"
+                      loading="lazy"
+                      alt=""
+                    />{" "}
+                  </div>
+                  <p>vs</p>
+                  <div className="awayTeam">
+                    <img
+                      src="https://cdn.jsdelivr.net/npm/country-flag-emoji-json@2.0.0/dist/images/AR.svg"
+                      loading="lazy"
+                      alt=""
+                    />{" "}
+                    Argentina
+                  </div>
+                </div>
+              </TableCell>
+              <TableCell align="center">
+                <Link to="/fixture">Fifa Worldcup</Link>
+              </TableCell>
+              <TableCell align="center">
+                <div className="userContent">
+                  <img
+                    src="https://avatars.githubusercontent.com/u/102910615?v=4"
+                    alt=""
+                  />
+                  <Link to="/profile">jckamaicha</Link>
+                </div>
+              </TableCell>
+              <TableCell align="center">21:45 PM, 20th Nov 2022</TableCell>
+              <TableCell align="center">{row.carbs}</TableCell>
+              <TableCell align="center">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- src/pages/Leaderboard/index.jsx
+++ src/pages/Leaderboard/index.jsx
@@ -1,8 +1,11 @@
+import { Pagination } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 import {
   getAllLeaderboards,
   getLeaderboardByMarketplaceSlug,
 } from "../../api/Leaderboards";
+import FixtureTable from "./components/FixtureTable";
 import "./styles/style.css";
 
 const Leaderboard = () => {
@@ -40,10 +43,7 @@ const Leaderboard = () => {
         <h1>Leaderboards</h1>
         <div className="search__container">
           <i className="ri-search-line icon"></i>
-          <input
-            type="input"
-            placeholder="Search Fixtures..."
-          />
+          <input type="input" placeholder="Search Fixtures..." />
         </div>
         <select name="" id="">
           <option value="">Select Marketplace</option>
@@ -52,7 +52,105 @@ const Leaderboard = () => {
         </select>
       </div>
       <div className="divider"></div>
-      
+
+      <main>
+        <div className="fixturesLeaderboard__container">
+          <h2>📈 Top Ranked Fixtures</h2>
+          <FixtureTable />
+          <Pagination style={{ marginTop: 15 }} count={10} />
+        </div>
+
+        <div className="usersLeaderboard__container">
+          <h2>⛹️ Top Ranked Users</h2>
+
+          <div className="topThreeUsers">
+            <div className="topUser">
+              <div className="user__avatar">
+                <img
+                  src="https://avatars.githubusercontent.com/u/102910615?v=4"
+                  loading="lazy"
+                />
+              </div>
+              <div className="user__info">
+                <h1>🥇1st Place</h1>
+                <h4>
+                  <Link to="/">Jessica, 0xb05c64...119e37</Link>
+                </h4>
+              </div>
+              <div className="gameDetails">
+                <div>
+                  <p>Wins</p>
+                  <span>12</span>
+                </div>
+                <div>
+                  <p>Games</p>
+                  <span>12</span>
+                </div>
+                <div>
+                  <p>Volume</p>
+                  <span>12</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="top3User">
+              <div className="user__avatar">
+                <img
+                  src="https://avatars.githubusercontent.com/u/102910615?v=4"
+                  loading="lazy"
+                />
+              </div>
+              <div className="user__info">
+                <h4>
+                  <Link to="/">🥈Jessica, 0xb05...9e37</Link>
+                </h4>
+                <div className="gameDetails">
+                  <div>
+                    <p>Wins</p>
+                    <span>12</span>
+                  </div>
+                  <div>
+                    <p>Games</p>
+                    <span>12</span>
+                  </div>
+                  <div>
+                    <p>Volume</p>
+                    <span>12</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="top3User">
+              <div className="user__avatar">
+                <img
+                  src="https://avatars.githubusercontent.com/u/102910615?v=4"
+                  loading="lazy"
+                />
+              </div>
+              <div className="user__info">
+                <h4>
+                  <Link to="/">🥉 Jessica, 0xb05...9e37</Link>
+                </h4>
+                <div className="gameDetails">
+                  <div>
+                    <p>Wins</p>
+                    <span>12</span>
+                  </div>
+                  <div>
+                    <p>Games</p>
+                    <span>12</span>
+                  </div>
+                  <div>
+                    <p>Volume</p>
+                    <span>12</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
