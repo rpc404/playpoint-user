c2934a7eed4d372a81f5aa1459f00ece05c7b225
surajgaire14 <eriag321@gmail.com>
2022-12-17 21:42:21 +0545

    changes in home page

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -7,6 +7,8 @@ const Hero = () => {
   return (
     <div className="hero__container">
       <div className="hero">
+        <div className="blob"></div>
+        <div className="blob1"></div>
         <div className="heroContent">
           <h1>Predict & Win</h1>
           <div className="card">
@@ -28,20 +30,11 @@ const Hero = () => {
             <div className="beginButton" tabIndex="0" role="button">
               Begin
             </div>
-            <p>Be our next winner!</p>
+            <p>Be the next winner!</p>
           </div>
         </div>
         <div className="heroImage">
           <img src={hero} alt="hero Image" />
-          <div className="blob">
-            {/* <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-              <path
-                fill="#2dabde87"
-                d="M20.9,14.8C9.5,31.8,-29.4,35.6,-37.5,20.5C-45.5,5.4,-22.8,-28.7,-3.3,-30.6C16.2,-32.5,32.3,-2.3,20.9,14.8Z"
-                transform="translate(100 100)"
-              />
-            </svg> */}
-          </div>
         </div>
       </div>
     </div>
--- src/components/Leaderboards/Leaderboards.jsx
+++ src/components/Leaderboards/Leaderboards.jsx
@@ -1,6 +1,6 @@
 import { Skeleton } from "@mui/material";
 import React from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import {
   getAllLeaderboards,
   getLeaderboardByMarketplaceSlug,
