7a72f2cef5facdd0747c9724f1a4cc4f510e0081
surajgaire14 <eriag321@gmail.com>
2022-12-27 19:42:36 +0545

    committed to pull from main

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 import "./styles/style.css";
-import hero from "../../images/new.png";
+// import hero from "../../images/new.png";
 import { Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import sports from "../../images/new.png";
 
 const Hero = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <div className="hero__container">
       <div className="blob"></div>
       <div className="blob1"></div>
-      <div className="heroIntroduction">
+      {/* <div className="heroIntroduction">
         <div className="heroContent">
           <h1>Predict & Win</h1>
           <div className="card">
-            {/* <Typography>Introduction!</Typography> */}
-            {/* <Typography variant="h2" component={"h2"}>
-              Start playing and Earning today!
-            </Typography> */}
             <Typography className="introduction">
               Playpoint is an online prediction pool platform based on real-time
               sporting events, supported by decentralized blockchain networks.
@@ -58,6 +55,61 @@ const Hero = () => {
           />
           <p>We are available in Arbitrum</p>
         </div>{" "}
+      </div> */}
+      <div className="heroSection">
+        <Typography component={"h1"} variant="h1">
+          Your one and only prediction platform
+        </Typography>
+        <Typography component={"h3"} variant="h3">
+          Predict & Win
+        </Typography>
+        <div className="buttons">
+          <button onClick={() => navigate("Marketplace")}> Get Started</button>
+          <button
+            onClick={() => window.open("https://docs.playpoint.ai/", "_blank")}
+          >
+            Learn More
+          </button>
+        </div>
+        <div className="img_container">
+          <img src={sports} alt="sports" />
+        </div>
+      </div>
+
+      {/* <div className="story__container">
+        <div className="story">
+          <Typography component={"p"} variant="h3">
+            Playpoint is an online prediction pool platform based on real-time
+            sporting events, supported by decentralized blockchain networks.
+          </Typography>
+        </div>
+      </div> */}
+      <div className="card__container">
+        {/* <div className="card1">
+          <Typography component={"h3"} variant="h3">
+            Our Story.
+          </Typography>
+          <Typography component={"p"} variant="p">
+            Playpoint is an online prediction pool platform based on real-time
+            sporting events, supported by decentralized blockchain networks.
+          </Typography>
+        </div> */}
+        <div className="card2">
+        <img
+          src="https://89devs.com/img/solidity/sepolia_dolphin.png"
+            alt="playpoint_logo"
+            loading="lazy"
+          />
+          <p>Currently we are testing on sepolia testnet.</p>
+        </div>
+        <div className="card3">
+        <img
+            src="https://assets-global.website-files.com/6364e65656ab107e465325d2/637ea09ce69ec8d049fb871e_nt4Cn4mhAnXmPqN4XNw7P3C8w59Mi18UQsJr0Kv-OWU.svg"
+            alt="playpoint_logo"
+            loading="lazy"
+          />
+          <p>We are available in Arbitrum</p>
+        </div>
       </div>
     </div>
   );
