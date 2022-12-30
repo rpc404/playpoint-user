86a6f9ca439125254f78aa12edf23ace9199c14e
sudeepmi <sudeepm0599@gmail.com>
2022-12-30 22:25:38 +0545

    session isssue resolved

--- src/pages/challenges/index.jsx
+++ src/pages/challenges/index.jsx
@@ -7,14 +7,15 @@ const Challenges = () => {
     <div className='challenges__Container'>
       <div className='challenges__UserChallenges'>
         <h2>My Active Challenges</h2>
-        {
+        {/* {
           [1,3,4,5,6,7].map(res=><p>challenge {res}</p>)
-        }
+        } */}
       </div>
 
       <div className='challenges__AllChallenges'>
         <h2>All Challenges</h2>
       </div>
+      <h2>Cooking !!! </h2>
     </div>
   )
 }
