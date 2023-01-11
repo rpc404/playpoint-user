bedd2ea9517f483672d4ff3d2a131db48a19ae6d
rpc404 <theboringschool.org@gmail.com>
2023-01-12 02:02:13 +0545

    Redeployment

--- src/components/LeaderboardList/Leaderboard.jsx
+++ src/components/LeaderboardList/Leaderboard.jsx
@@ -1,22 +1,22 @@
 import Leaderboards from "../Leaderboards/Leaderboards";
 
 const LeaderBoardList = ({ fixture, open, marketplaceSlug }) => {
-    return (
-      <div className={`leaderboards `}>
-        <h3 className="leaderboardsTitle">Leaderboards</h3>
-        <div className="leaderboardItemsTitle">
-          <p>
-            Game<i className="ri-game-line"></i>
-          </p>
-          <p>
-            Users<i className="ri-magic-line"></i>
-          </p>
-          <p>
-            Volume<i className="ri-money-dollar-circle-line"></i>
-          </p>
-        </div>
-        {fixture && <Leaderboards marketplaceSlug={marketplaceSlug} />}{" "}
-        {/* {fixture[0].length >=1 && fixture.map((data) => {
+  return (
+    <div className={`leaderboards `}>
+      <h3 className="leaderboardsTitle">Leaderboards</h3>
+      <div className="leaderboardItemsTitle">
+        <p>
+          Game<i className="ri-game-line"></i>
+        </p>
+        <p>
+          Users<i className="ri-magic-line"></i>
+        </p>
+        <p>
+          Volume<i className="ri-money-dollar-circle-line"></i>
+        </p>
+      </div>
+      {fixture && <Leaderboards marketplaceSlug={marketplaceSlug} />}{" "}
+      {/* {fixture[0].length >=1 && fixture.map((data) => {
           return (
             <div className="leaderboardItem__container" key={data}>
               <p>AUS/QTR</p>
@@ -25,8 +25,8 @@ const LeaderBoardList = ({ fixture, open, marketplaceSlug }) => {
             </div>
           );
         })} */}
-      </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default LeaderBoardList
+export default LeaderBoardList;
