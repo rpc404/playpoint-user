b3ebe2598aeffcce7b8bf894cecb953d9533d82c
suraj gaire <eriag321@gmail.com>
2023-01-12 23:31:19 +0545

    loader added

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const loadingContainer = {
+  width: "4em",
+  height: "4em",
+  display: "flex",
+  justifyContent: "space-around",
+  alignItems: "center;",
+};
+const loadingCircle = {
+  display: "block",
+  width: "1rem",
+  height: "1rem",
+  backgroundColor: "#fff9",
+  borderRadius: "0.5rem",
+};
+
+const loadingContainerVariants = {
+  start: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+  end: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const loadingCircleVariants = {
+  start: {
+    y: "0%",
+  },
+  end: {
+    y: "60%",
+  },
+};
+const loadingCircleTransition = {
+  duration: 0.4,
+  yoyo: Infinity,
+  ease: "easeInOut",
+};
+
+const Loader = () => {
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          height: "100vh",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <motion.div
+          style={loadingContainer}
+          variants={loadingContainerVariants}
+          initial="start"
+          animate="end"
+        >
+          <motion.span
+            style={loadingCircle}
+            variants={loadingCircleVariants}
+            transition={loadingCircleTransition}
+          ></motion.span>
+          <motion.span
+            style={loadingCircle}
+            variants={loadingCircleVariants}
+            transition={loadingCircleTransition}
+          ></motion.span>
+          <motion.span
+            style={loadingCircle}
+            variants={loadingCircleVariants}
+            transition={loadingCircleTransition}
+          ></motion.span>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
