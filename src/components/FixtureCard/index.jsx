881c95a358fb16e784162d18989cbceb87fce56d
suraj gaire <eriag321@gmail.com>
2023-01-21 21:26:26 +0545

    status added in fixture page

--- src/components/FixtureCard/index.jsx
+++ src/components/FixtureCard/index.jsx
@@ -4,6 +4,7 @@ import CountryFlags from "../../helpers/CountryFlags.json";
 import { useNavigate } from "react-router-dom";
 import moment from "moment";
 import GetFlags from "../../utils/GetFlags";
+import useWindowDimensions from "../../helpers/UseWindowDimension";
 
 export default function FixtureCard({
   data,
@@ -44,21 +45,32 @@ export default function FixtureCard({
       </div>
     );
   };
-
+  const { width } = useWindowDimensions();
   return (
     <div className="fixtureCard__container">
       <Typography component={"span"} variant={"h3"}>
-      🗓️ {data}
+        🗓️ {data}
       </Typography>
 
       {getGamesByDate(data)
         .reverse()
         .map((data, index) => {
+          console.log(data);
           if (group === "all") {
             return (
               <div className="gameDetails__item" key={index}>
-                {window.innerWidth > 576 ? (
+                {
+                  <div className={`status ${data.status[0].status}`}>
+                    <p>{data.status[0].status}</p>
+                    {data.status[0].status === "open" && (
+                      <p className="dot"></p>
+                    )}
+                  </div>
+                }
+                <div className="gameDetails">
                   <div
+                    className="gameDetails__teamDetails"
+                    style={{ cursor: "pointer" }}
                     onClick={() =>
                       navigate(`/predict/${data?._id}`, {
                         state: {
@@ -66,55 +78,36 @@ export default function FixtureCard({
                         },
                       })
                     }
-                    className="gameDetails__teamDetails"
-                    style={{ cursor: "pointer" }}
                   >
                     <div className="teamName">{data?.HomeTeam}</div>
-                    {GetFlags(marketplaceSlug, data.HomeTeam)}
+                    {GetFlags(marketplaceSlug, data?.HomeTeam)}
                     {gameTime(data)}
                     {GetFlags(marketplaceSlug, data.AwayTeam)}
                     <div className="teamName">{data?.AwayTeam}</div>
                   </div>
-                ) : (
-                  <Button
-                    className="gameDetails__teamDetails"
-                    onClick={() =>
-                      navigate(`/predict/${data?._id}`, {
-                        state: {
-                          marketplaceSlug,
-                        },
-                      })
-                    }
-                  >
-                    <div className="teamName">{data?.HomeTeam}</div>
-                    {GetFlags(marketplaceSlug, data.HomeTeam)}
-                    {gameTime(data)}
-                    {GetFlags(marketplaceSlug, data.AwayTeam)}
-                    <div className="teamName">{data?.AwayTeam}</div>
-                  </Button>
-                )}
 
-                <div className="gameDetails__location">
-                  <i className="ri-map-pin-2-line"></i> {data?.Location}
-                </div>
+                  <div className="gameDetails__location">
+                    <i className="ri-map-pin-2-line"></i> {data?.Location}
+                  </div>
 
-                <div className="gameDetails__action">
-                  <Button
-                    className="quickView"
-                    onClick={() => handleModalOpen(data)}
-                  >
-                    Quick View
-                  </Button>
-                  <Button>
-                    <i className="ri-arrow-right-line"></i>
-                  </Button>
+                  <div className="gameDetails__action">
+                    <Button
+                      className="quickView"
+                      onClick={() => handleModalOpen(data)}
+                    >
+                      Quick View
+                    </Button>
+                    <Button>
+                      <i className="ri-arrow-right-line"></i>
+                    </Button>
+                  </div>
                 </div>
               </div>
             );
           } else if (data?.Group === group)
             return (
               <div className="gameDetails__item" key={index}>
-                {window.innerWidth > 576 ? (
+                {width > 576 ? (
                   <div
                     className="gameDetails__teamDetails"
                     onClick={() => navigate(`/predict/${data?._id}`)}
