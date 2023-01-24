2d47022b1a2ad9a27aa012fc8f16d34ccc83361a
suraj gaire <eriag321@gmail.com>
2023-01-24 20:19:47 +0545

    i18next key only showing

--- src/i18n.jsx
+++ src/i18n.jsx
@@ -3,8 +3,9 @@ import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-const apiKey = import.meta.env.VITE_i18n_API_KEY;
+const apiKey = "LXWpCfjUQcjnwjcDmtVwzA";
 const loadPath = `https://api.i18nexus.com/project_resources/translations/{{lng}}/{{ns}}.json?api_key=${apiKey}`;
+console.log(apiKey);
 
 i18next
   .use(HttpBackend)
