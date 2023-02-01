8410eec27b4b5a712459836c9d1b4d1cbaaca329
suraj gaire <eriag321@gmail.com>
2023-02-01 09:58:22 +0545

    output chunks changed in vite config

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,18 @@ export default defineConfig({
   build: {
     sourcemap: true,
     target: "esnext",
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split(".").at(1);
+          if (/svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = "svg";
+          }
+          return `assets/${extType}/[name].[ext]`;
+        },
+      },
+    },
   },
 });
