ea7fbdfe94b98cecd1c3e9df95ec0356d9353992
sudeepmi <sudeepm0599@gmail.com>
2023-02-11 17:24:00 +0545

    buffer added

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,13 @@ import react from "@vitejs/plugin-react";
 import autoprefixer from "autoprefixer";
 import cssnanoPlugin from "cssnano";
 import { NgmiPolyfill } from "vite-plugin-ngmi-polyfill";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),NgmiPolyfill()],
+  plugins: [react()],
   css: {
     postcss: {
       plugins: [autoprefixer({}), cssnanoPlugin()],
@@ -28,6 +31,27 @@ export default defineConfig({
           return `assets/${extType}/[name].[ext]`;
         },
       },
+      plugins: [
+        // Enable rollup polyfills plugin
+        // used during production bundling
+        NgmiPolyfill()
+    ]
     },
   },
+  optimizeDeps: {
+    esbuildOptions: {
+        // Node.js global to browser globalThis
+        define: {
+            global: 'globalThis'
+        },
+        // Enable esbuild polyfill plugins
+        plugins: [
+            NodeGlobalsPolyfillPlugin({
+                process: true,
+                buffer: true
+            }),
+            NodeModulesPolyfillPlugin()
+        ]
+    }
+},
 });
